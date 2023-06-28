c114f16c03983c0e9fbf11e72fd8e587064609a4
Edgar Juan Guercio <jguercio@live.com.ar>
2023-06-28 17:25:07 -0300

    aplicando estilos task, form, navBar

--- front/src/components/NavBar/NavBar.jsx
+++ front/src/components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar=()=>{
 
     return (
         <div className={css.navContainer}>
-         <NavLink to="/" className={css.title} >TO-DO PERN STACK</NavLink>
+         <NavLink to="/" className={css.title} >TO-DO HOME</NavLink>
          <NavLink to="/form"><button className={css.button}>CREATE TASK</button></NavLink>
         </div>
     )
