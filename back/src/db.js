eca47d93ab431e2a6596181187e4db88f5cf18b9
Edgar Juan Guercio <jguercio@live.com.ar>
2024-01-15 20:23:07 -0300

    EDIT base de datos en vercel

--- back/src/db.js
+++ back/src/db.js
@@ -1,12 +1,23 @@
 const { Pool } = require("pg");
 require("dotenv").config();
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+//const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
+// const pool = new Pool({
+//     user: DB_USER,
+//     password: DB_PASSWORD,
+//     host: DB_HOST,
+//     port: DB_PORT,
+//     database: DB_NAME
+// })
 const pool = new Pool({
-    user: DB_USER,
-    password: DB_PASSWORD,
-    host: DB_HOST,
-    port: DB_PORT,
-    database: DB_NAME
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+})
+
+pool.connect((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Connected to database");
+    }
 })
 module.exports = pool;
