4d98825219392c652fb25edcbc5ad7eb46c54098
Edgar Juan Guercio <jguercio@live.com.ar>
2024-01-15 21:25:40 -0300

    NEW cors configuration

--- back/src/app.js
+++ back/src/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors =require("cors")
+const cors = require("cors")
 const routes = require("./routes/index");
 
 const server = express();
 //MIDDLEWARES
-server.use(cors()); //comunicar front y back
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});; //comunicar front y back
 server.use(morgan("dev"));
 server.use(express.json());
 
