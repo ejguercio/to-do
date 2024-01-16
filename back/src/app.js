bc3ee7d8355dd642757aedfae310116f10346c3d
Edgar Juan Guercio <jguercio@live.com.ar>
2024-01-16 20:29:09 -0300

    EDIT cors

--- back/src/app.js
+++ back/src/app.js
@@ -5,13 +5,7 @@ const routes = require("./routes/index");
 
 const server = express();
 //MIDDLEWARES
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://back-navy-psi.vercel.app/'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});; //comunicar front y back
+server.use(cors()); //comunicar front y back
 server.use(morgan("dev"));
 server.use(express.json());
 
