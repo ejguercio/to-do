b255c013ce0387d394db20de0d10ede21877eb88
Edgar Juan Guercio <jguercio@live.com.ar>
2024-01-17 01:24:33 -0300

    FEAT paginado backend

--- back/src/handlers/getAllTasks.js
+++ back/src/handlers/getAllTasks.js
@@ -1,11 +1,23 @@
 const pool = require("../db");
+const pageSize = 4;
 
 const getAllTasks = async (req, res) => {
     try {
-        const result = await pool.query("SELECT * FROM task ORDER BY id")
-        return res.status(200).json(result.rows)
+        // Parámetros de paginación
+        // Si no hubiese query page devolvemos la primer pagina
+        const { page = 1 } = req.query;
+        const offset = (page - 1) * pageSize;
+
+        // Consulta con paginación
+        const result = await pool.query("SELECT * FROM task ORDER BY id OFFSET $1 LIMIT $2", [offset, pageSize]);
+
+        // Consulta para contar el total de tareas
+        const totalCountResult = await pool.query("SELECT COUNT(*) FROM task");
+        const totalCount = parseInt(totalCountResult.rows[0].count, 10);
+
+        return res.status(200).json({ taksPerPage: result.rows, totalTaks: totalCount });
     } catch (error) {
-        return res.status(400).json({ error: error.message })
+        return res.status(400).json({ error: error.message });
     }
 };
 
